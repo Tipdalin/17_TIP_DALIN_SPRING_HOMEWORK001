7f20263d0059218308171d60c68a4ae8dc6b37d6
Tip Dalin <www.tipdalin11@gmail.com>
2026-09-17 03:17:38 +0700

    add version2 endpoint

--- src/main/java/org/example/ticketrestapi/controller/TicketController.java
+++ src/main/java/org/example/ticketrestapi/controller/TicketController.java
@@ -1,6 +1,5 @@
 package org.example.ticketrestapi.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +16,10 @@ public class TicketController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello from Ticket API";
+    }
+
+    @GetMapping("/v2")
+    public String version2() {
+        return "Ticket API Version 2";
     }
 }
