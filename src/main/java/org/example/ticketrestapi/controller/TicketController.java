0e4a38982a02af1773f7da09879b054675b0d464
Tip Dalin <www.tipdalin11@gmail.com>
2026-09-16 13:46:08 +0700

    get method

---
+++ src/main/java/org/example/ticketrestapi/controller/TicketController.java
@@ -0,0 +1,21 @@
+package org.example.ticketrestapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tickets")
+public class TicketController {
+
+    @GetMapping
+    public String getTickets() {
+        return "Ticket API is working";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Ticket API";
+    }
+}
